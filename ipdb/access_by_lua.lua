baa3c8b8733fb57bc6a2162a82434f097bff6665
deancn <dean@airdb.com>
2021-10-22 02:51:11 +0800

    update

--- ipdb/access_by_lua.lua
+++ ipdb/access_by_lua.lua
@@ -1,11 +1,18 @@
-local args = ngx.req.get_uri_args();
+-- ngx.say(ngx.var.remote_addr);
+-- return ngx.exit(200);
 
-local loc = ipdb:find(args["ip"], "EN");
-#ngx.say(cjson.encode(loc));
+local cip = ngx.var.remote_addr
+-- local cip = ngx.var.http_x_forwarded_for
+
+local args = ngx.req.get_uri_args();
+-- local loc = ipdb:find(args["ip"], "EN");
+
+local loc = ipdb:find(cip, "EN");
+-- ngx.say(cjson.encode(loc));
 
 if loc.idc == "IDC" then
-	ngx.log(ngx.ERR, args["ip"], loc.idc);
-	#ngx.say("access");
+	ngx.log(ngx.ERR, cip, loc.idc);
+	-- ngx.say("access");
 	return ngx.exit(500);
 end
 
--- ipdb/main.lua
+++ ipdb/main.lua
@@ -1,12 +1,19 @@
-local args = ngx.req.get_uri_args();
+-- local cip = ngx.var.remote_addr
+-- local cip = ngx.var.http_x_forwarded_for
 
-local loc = ipdb:find(args["ip"], "EN");
+local args = ngx.req.get_uri_args();
+local cip = args["ip"]
+-- ngx.say(cip);
+-- local loc = ipdb:find(args["ip"], "EN");
+
+local loc = ipdb:find(cip, "EN");
+ngx.say(cjson.encode(loc));
+
 if loc.idc == "IDC" then
-	ngx.log(ngx.ERR, args["ip"], loc.idc);
+	ngx.log(ngx.ERR, cip, loc.idc);
+	-- ngx.say("access");
 	return ngx.exit(500);
 end
-
-#ngx.say(cjson.encode(loc));
 
 -- ngx.say(args["ip"]);
 -- ngx.say("hello\n");
